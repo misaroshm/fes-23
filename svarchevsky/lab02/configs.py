6611d29d8419fac81dcc7aa5aef8449870267fbe
goga564 <102030278+goga564@users.noreply.github.com>
2023-05-31 09:46:40 +0300

    configs.py

---
+++ svarchevsky/lab02/configs.py
@@ -0,0 +1,11 @@
+"""Consists of configs for modeling bee colony"""
+
+
+class Configs:
+    SEARCH_REGION_HEIGHT = 100
+    SEARCH_REGION_WIDTH = 100
+    COLONY_SIZE = 50
+    DIMENSION = 2
+    SOURCES = 50
+    BASE_AGENT_CAPACITY = 5
+    WORKER_LIMIT_ON_SOURCE = 3
